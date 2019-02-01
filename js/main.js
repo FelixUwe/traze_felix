cb49cece9cf6991cdd3f9ab2385476ce08ceec75
Felix Heinenbruch <felix.heinenbruch@gmail.com>
2019-02-01 15:46:56 +0100

    Spawnpunkt blinkt, deadTrail wird nicht angezeigt aber beim Tod blinkt man nicht auf

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const url = 'wss://traze.iteratec.de:9443';
-const tiles = 62;
+const tiles = -1;
 const clientId = pickNewID();
 let playerId = '';
 
@@ -7,7 +7,8 @@ const topics = [
     'traze/1/players',
     'traze/1/grid',
     'traze/1/join',
-    'traze/1/player/' + clientId
+    'traze/1/player/' + clientId,
+    'traze/1/ticker'
 ];
 
 
@@ -22,13 +23,8 @@ let secretToken = '';
 let steerTopic = '';
 let bailTopic = '';
 
-let i = '';
-let j = '';
-
 let id_to_color = {};
 let id_to_position = {};
-
-let cell = '';
 
 let spielerFarben = {};
 let playerCount = '';
@@ -44,22 +40,17 @@ client.on('connect', function () {
 client.on('message', function (topic, message) {
     if (topic === topics[3]) {
         message = JSON.parse(message);
-        console.log(message);
         secretToken = message.secretUserToken;
         playerId = message.id;
         steerTopic = 'traze/1/' + playerId + '/steer';
         bailTopic = 'traze/1/' + playerId + '/bail';
-        console.log(playerId);
-
-
         playerInformation();
     }
 
     if (topic === topics[0]) {
         playerMessage = JSON.parse(message);
         spielerAnzahl = playerMessage.length;
-        // console.log(spielerAnzahl);
-        for (i = 0; i < spielerAnzahl; i++) {
+        for (let i = 0; i < spielerAnzahl; i++) {
             playerCount = i;
             // console.log(playerCount);
         }
@@ -69,19 +60,11 @@ client.on('message', function (topic, message) {
     if (topic === topics[1]) {
         gridMessage = JSON.parse(message);
         drawPlayer(gridMessage);
-        //console.log(gridMessage);
-
-        // gridErstellen(gridMessage);
-        // console.log(gridMessage.bikes[0].currentLocation);
-        // let position = gridMessage.bikes[0].currentLocation;
-        // console.log(position);
-        //
-        // for (player of playerMessage) {
-        // console.log(playerMessage[0].color);
-        //     let player = gridMessage.bikes;
-        //     // console.log(player);
-        // }
-
+        paintSpawnPoint(gridMessage);
+    }
+    if (topic === topics[4]) {
+        tickerMessage = JSON.parse(message);
+        onTickerMessage(tickerMessage);
     }
 
 
@@ -90,18 +73,49 @@ client.on('message', function (topic, message) {
 function drawPlayer(gridMessage) {
     for (let x = 0; x < gridMessage.tiles.length; x++) {
         for (let y = 0; y < gridMessage.tiles.length; y++) {
-             playerId = gridMessage.tiles[x][y];
-            cell = document.getElementById(x+"-"+y);
-            if(cell)
-            {
+            let playerId = gridMessage.tiles[x][y];
+            let cell = document.getElementById(x+"-"+y);
+            if (cell.classList.contains("blink")) {
+                cell.classList.remove("blink");
+            }
+            if(cell) {
                 cell.style.background = id_to_color[playerId];
             }
             else {
                 console.log("cell " + x + "-" + y + " not found");
             }
+
         }
-        //wenn currentLocation != auf dem Spielfeld Spieler nicht anzeigen o. wenn id = 0 zeige farbe schwarz an
         
+    }
+}
+
+function onTickerMessage(tickerMessage) {
+    let deadId = tickerMessage.casualty;
+    let deadTrail;
+    for (let i = 0; i < gridMessage.bikes.length; i++) {
+        if (deadId = gridMessage.bikes[i].playerId) {
+            deadTrail = gridMessage.bikes[i].trail;
+            deadTrail.push(gridMessage.bikes[i].currentLocation);
+            paintTrailWhenKilled(deadTrail);
+        }
+    }
+
+}
+
+function paintTrailWhenKilled(deadTrail) {
+    for (let i = 0; i < deadTrail.length; i++) {
+        let cell = document.getElementById(deadTrail[i][0] + "-" + deadTrail[i][1]);
+        cell.style.background = "white";
+    }
+}
+
+function paintSpawnPoint(gridMessage) {
+    let spawnPoints = gridMessage.spawns;
+    for (let i = 0;i < gridMessage.spawns.length; i++) {
+        let cell = document.getElementById(spawnPoints[i][0] + "-" + spawnPoints[i][1]);
+        cell.classList.add("blink");
+        // cell.style.background = "black";
     }
 }
 
@@ -119,15 +133,10 @@ function playerInformation(){
         document.getElementById('tf1').innerText += player.name + '\n';
         document.getElementById('tf2').innerText += player.frags + '\n';
 
-        // map player id to color every 5 seconds (on player topic)
         id_to_color[player.id] = player.color;
     }
 
-    // document.getElementById('feld2').innerHTML = tickerMessage;
-    // document.getElementById('feld3').innerHTML = gridMessage;
 
-    //console.log(playerMessage);
-    // document.getElementById('feld1').innerHTML = playerInformation;
 }
 
 
@@ -142,7 +151,6 @@ function joinGame() {
     steuerInput();
 
     client.publish(topics[2], JSON.stringify(joinMsg));
-    console.log(topics[2]);
     //
 }
 
@@ -153,6 +161,7 @@ function test() {
     console.log(playerMessage);
     console.log(gridMessage);
     console.log(cell);
+    console.log(logCell);
 
 }
 
@@ -203,15 +212,13 @@ function untenSteuern() {
 
 function leave() {
     let bailMessage = {playerToken : secretToken};
-    console.log(bailTopic);
-    console.log(bailMessage);
     client.publish(bailTopic, JSON.stringify(bailMessage));
 }
 
 function pickNewID() {
     let d = new Date().getTime();
     if(Date.now){
-        d = Date.now(); //high-precision timer
+        d = Date.now();
     }
     let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
         let r = (d + Math.random()*16)%16 | 0;
@@ -226,13 +233,9 @@ function showGrid() {
 
     let spielfeld = document.getElementById("Spielfeld");
     let div = document.getElementsByTagName("span");
-    // let div2 = document.getElementsByTagName("span");
-
-    // console.log(gridMessage);
-    for ( i = 0; i < tiles; i++) {
-        // div.id
+    for (let j = 61; j > tiles; j--) {
         let row = document.createElement("div");
-        for ( j = 0; j < tiles; j++) {
+        for (let i = 61; i > tiles; i--) {
             let cell = document.createElement("span");
             cell.id = "" + i + "-" + j;
             row.appendChild(cell);
