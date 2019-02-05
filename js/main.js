7f22014254c194b7691b50174559d58f74a3ba8b
Felix Heinenbruch <felix.heinenbruch@gmail.com>
2019-02-05 14:40:15 +0100

    first steps

--- js/main.js
+++ js/main.js
@@ -17,6 +17,12 @@ let playerId, secretToken, steerTopic, bailTopic, playerCount, deadId;
 
 let id_to_color = {0: 'BLACK'};
 
+let botMode = false;
+
+let x = '';
+
+let aktuellerBotBike = '';
+
 CLIENT.on('connect', function () {
     let topics = Object.keys(TOPIC_TO_FUNCTION);
     CLIENT.subscribe(topics, function (err) {
@@ -37,6 +43,9 @@ function onPlayers(message) {
 function onMyPlayer(message) {
     secretToken = message.secretUserToken;
     playerId = message.id;
+
+    console.log("set id to ", playerId);
+
     steerTopic = 'traze/1/' + playerId + '/steer';
     bailTopic = 'traze/1/' + playerId + '/bail';
     // playerInformation(message);
@@ -46,6 +55,16 @@ function onMyPlayer(message) {
 function onGrid(message) {
     drawPlayer(message);
     paintSpawnPoint(message);
+
+    if (botMode) {
+        console.log("Grid: Bot mode!");
+        if (aktuellerBotBike === '') {
+            console.log("Grid: no bike found!");
+            setBotBike(message)
+        } else {
+            botSteuernRechtsWennRechtsFrei(message);
+        }
+    }
 }
 
 function onTicker(message) {
@@ -59,27 +78,28 @@ CLIENT.on('message', function (topic, message) {
 });
 
 function drawPlayer(gridMessage) {
+
     for (let x = 0; x < gridMessage.tiles.length; x++) {
         for (let y = 0; y < gridMessage.tiles.length; y++) {
             let playerId = gridMessage.tiles[x][y];
             let cell = document.getElementById(x + "-" + y);
-
             if (cell.classList.contains("blink")) {
+
                 cell.classList.remove("blink");
             }
             if (cell) {
                 cell.style.background = id_to_color[playerId];
-            }
-            else {
+            } else {
                 console.error("cell " + x + "-" + y + " not found");
             }
-
         }
 
     }
+
 }
 
 function paintSpawnPoint(gridMessage) {
+
     let spawnPoints = gridMessage.spawns;
     for (let i = 0; i < gridMessage.spawns.length; i++) {
         let cell = document.getElementById(spawnPoints[i][0] + "-" + spawnPoints[i][1]);
@@ -88,21 +108,22 @@ function paintSpawnPoint(gridMessage) {
 }
 
 function playerInformation(message) {
+
     document.getElementById('tf1').innerText = '';
     document.getElementById('tf2').innerText = '';
-
     for (player of message) {
+
         document.getElementById('tf1').innerText += player.name + '\n';
         document.getElementById('tf2').innerText += player.frags + '\n';
-
         id_to_color[player.id] = player.color;
+
     }
 }
 
-
 function joinGame() {
-    let playerName = document.getElementById("playerNameInput").value;
+    botMode = false;
 
+    let playerName = document.getElementById("playerNameInput").value;
     let joinMsg = {
         name: playerName,
         mqttClientName: CLIENT_ID
@@ -114,22 +135,37 @@ function joinGame() {
 }
 
 function toderkennen() {
+
+    let steerButtonList = document.getElementsByClassName("steer-button");
+
+    let index = '';
+
     if (deadId == playerId) {
         document.removeEventListener('keydown', eventListener);
+        for (let index = 0; index < steerButtonList.length; index++) {
+            let steerButton = steerButtonList[index];
+            if (steerButton.classList.contains("deaktiviert")) {
+                steerButton.removeAttribute("disabled");
+                steerButton.classList.remove("deaktiviert");
+            }
+        }
+        botMode = false;
     }
 
 }
 
 function test() {
-    console.log("TOPIC_TO_FUNCTION",TOPIC_TO_FUNCTION);
-    console.log("playerId",playerId);
+    console.log("TOPIC_TO_FUNCTION", TOPIC_TO_FUNCTION);
+    console.log("playerId", playerId);
 
 }
+
 function steuern(richtung) {
     let steuernMessage = {course: richtung, playerToken: secretToken};
     CLIENT.publish(steerTopic, JSON.stringify(steuernMessage));
 
 }
+
 function registerKeyboardInputs() {
     document.addEventListener('keydown', eventListener);
 }
@@ -156,7 +192,13 @@ function eventListener(event) {
 function selbstmordBot() {
     let selbstmordBotName = "SelbstmordBot";
 
-   let clientId = pickNewID();
+    let steerButtonList = document.getElementsByClassName("steer-button");
+
+    for (let index = 0; index < steerButtonList.length; index++) {
+        let steerButton = steerButtonList[index];
+        steerButton.classList.add("deaktiviert");
+        steerButton.setAttribute("disabled", "disabled");
+    }
 
     let joinMsg = {
         name: selbstmordBotName,
@@ -164,10 +206,82 @@ function selbstmordBot() {
     };
 
     CLIENT.publish(JOIN_TOPIC, JSON.stringify(joinMsg));
-
-    setTimeout(function() {
+    setTimeout(function () {
         steuern('E');
     }, 1000);
+
+}
+
+function bot() {
+
+    let botName = "Superbot";
+
+    let steerButtonList = document.getElementsByClassName("steer-button");
+
+    for (let index = 0; index < steerButtonList.length; index++) {
+        let steerButton = steerButtonList[index];
+        steerButton.classList.add("deaktiviert");
+        steerButton.setAttribute("disabled", "disabled");
+    }
+
+    let joinMsg = {
+        name: botName,
+        mqttClientName: CLIENT_ID
+    };
+
+    console.log("Bot wants to join...");
+    CLIENT.publish(JOIN_TOPIC, JSON.stringify(joinMsg));
+
+    setTimeout(function () {
+        botMode = true;
+        console.log("activate bot mode!");
+        steuern('W');
+    }, 1000);
+}
+
+function setBotBike(message) {
+    for (let x = 0; x < message.bikes.length; x++) {
+        if (playerId === message.bikes[x].playerId) {
+            aktuellerBotBike = message.bikes[x];
+        }
+    }
+    console.log("aktuellerBotBike", aktuellerBotBike);
+}
+
+function botSteuernRechtsWennRechtsFrei(message) {
+    let naechstePositionRechts = "" + aktuellerBotBike.currentLocation[0] + 1 + "-" + aktuellerBotBike.currentLocation[1];
+    if (naechstePositionRechts === document.getElementById(naechstePositionRechts) && document.getElementById(naechstePositionRechts).style.background === "black") {
+        steuern('E');
+    } else {
+        botSteuernLinksWennLinksFrei(message);
+    }
+}
+
+function botSteuernLinksWennLinksFrei(message) {
+    let naechstePositionLinks = "" + aktuellerBotBike.currentLocation[0] - 1 + "" +aktuellerBotBike.currentLocation[1];
+
+    if (naechstePositionLinks === document.getElementById(naechstePositionLinks) && document.getElementById(naechstePositionLinks).style.background === "black") {
+        steuern('W');
+    } else {
+        botSteuernUntenWennUntenFrei(message);
+    }
+}
+
+function botSteuernUntenWennUntenFrei(message) {
+    let naechstePositionUnten = "" + aktuellerBotBike.currentLocation + "-" + aktuellerBotBike.currentLocation[1] - 1;
+    if (naechstePositionUnten === document.getElementById(naechstePositionUnten) && document.getElementById(naechstePositionUnten).style.background === "black") {
+        steuern('S');
+    } else {
+        botSteuernObenWennObenFrei(message);
+    }
+}
+
+function botSteuernObenWennObenFrei(message) {
+    let naechstePositionOben = "" + aktuellerBotBike.currentLocation + "-" + aktuellerBotBike.currentLocation[1] + 1;
+    if (naechstePositionOben === document.getElementById(naechstePositionOben) && document.getElementById(naechstePositionOben).style.background === "black") {
+        steuern('N');
+    }
+
 }
 
 function leave() {
